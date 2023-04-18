d63778cd6ebc767d25fa6e20c17085154639297c
Clemente <clemente.rodriguezre@elorrieta-errekamari.com>
2023-04-18 10:27:26 +0200

    commit 2

--- Grupo2Reto4/src/Modelo/Pokemon.java
+++ Grupo2Reto4/src/Modelo/Pokemon.java
@@ -4,6 +4,12 @@ public class Pokemon {
 	
 	private int id;
 	private String nombre_pokemon;
-	private Tipo[] tipo= 
+	private Tipo[] tipo;
+	private int hp;
+	private int att;
+	private int def;
+	private int satt;
+	private int sdef;
+	private int vel;
 	
 }
