0fb95c43348de311534b74ab9ab6bb1b15590ebe
Hodei <hodei.martinezag@elorrieta-errekamari.com>
2023-04-20 13:45:11 +0200

    commit login

--- Grupo2Reto4/src/vista/VistaLogin.java
+++ Grupo2Reto4/src/vista/VistaLogin.java
@@ -51,14 +51,14 @@ public class VistaLogin extends JFrame implements ActionListener {
 	 */
 	public VistaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 647, 386);
+		setBounds(100, 100, 610, 279);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		loginTF = new JTextField();
-		loginTF.setBounds(146, 96, 139, 20);
+		loginTF.setBounds(144, 62, 139, 20);
 		contentPane.add(loginTF);
 		loginTF.setColumns(10);
 
@@ -68,22 +68,23 @@ public class VistaLogin extends JFrame implements ActionListener {
 		contentPane.add(atras);
 
 		passw1TF = new JPasswordField();
-		passw1TF.setBounds(146, 168, 139, 20);
+		passw1TF.setBounds(144, 134, 139, 20);
 		contentPane.add(passw1TF);
 
-		btnLogin = new JButton("Abrir sesion");
-		btnLogin.setBounds(10, 128, 112, 23);
+		btnLogin = new JButton("Iniciar sesion");
+		btnLogin.addActionListener(this);
+		btnLogin.setBounds(10, 98, 122, 23);
 		contentPane.add(btnLogin);
 
 		errLogin = new JLabel("");
 		errLogin.setForeground(Color.RED);
-		errLogin.setBounds(323, 99, 228, 14);
+		errLogin.setBounds(321, 65, 228, 14);
 		contentPane.add(errLogin);
 		errLogin.setVisible(false);
 
 		errPassw = new JLabel("");
 		errPassw.setForeground(Color.RED);
-		errPassw.setBounds(323, 171, 228, 14);
+		errPassw.setBounds(321, 137, 228, 14);
 		contentPane.add(errPassw);
 		errPassw.setVisible(false);
 	}
@@ -98,10 +99,16 @@ public class VistaLogin extends JFrame implements ActionListener {
 				errLogin.setVisible(true);
 				errLogin.setText("Este campo no puede esar vacio.");
 			}
+			else {
+				errLogin.setVisible(false);
+			}
 
 			if (metodos.esVacio(String.valueOf(passw1TF.getPassword()))) {
 				errPassw.setVisible(true);
 				errPassw.setText("La contraseña no puede estar vacia.");
+			}
+			else {
+				errPassw.setVisible(false);
 			}
 		}
 	}
