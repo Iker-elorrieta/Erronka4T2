cf6dab180c92dab65cd73befb69f349a07f26ea8
AKOS-PC <AKOS-PC@PC-UNAI>
2023-04-26 16:28:38 +0200

    test Caja y Jugador (Revisar Jugador en clase, errores de mi version)

--- Grupo2Reto4/src/test/CajaTest.java
+++ Grupo2Reto4/src/test/CajaTest.java
@@ -12,46 +12,61 @@ import modelo.Pokemon;
 import modelo.Tipo;
 
 class CajaTest {
-
+	Tipo[] tipos = new Tipo[2];
+	Tipo planta = new Tipo(3,"Planta");
+	Tipo veneno = new Tipo(12,"Veneno");
+	Tipo normal = new Tipo(1,"Normal");
+	Movimiento move1 = new Movimiento(1,"Latigo cepa",15,100,planta, 50);
+	Movimiento move2 = new Movimiento(2,"Placaje",40,100,normal, 40);
+	Movimiento move3 = new Movimiento(3,"Absorber",20,100,planta, 20);
+	Movimiento move4 = new Movimiento(4,"Bomba Lodo",15,100,veneno, 90);
+	ArrayList<Movimiento> moveset = new ArrayList<Movimiento>();
+	ArrayList<Movimiento> moveset2 = new ArrayList<Movimiento>();
+	ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
+	Caja caja = new Caja(1, pokemon);
+	Caja caja2 = new Caja(2, pokemon);
+	Caja caja3 = new Caja(2, pokemon);
 	@Test
-	void testCaja() {
-		
-		Tipo[] tipos = new Tipo[2];
-		Tipo planta = new Tipo(3,"Planta");
-		Tipo veneno = new Tipo(12,"Veneno");
-		Tipo normal = new Tipo(1,"Normal");
+	void testCajaConstructor() {
 		tipos[0] = planta;
 		tipos[1] = veneno;
-		ArrayList<Movimiento> moveset = new ArrayList<Movimiento>();
-		Movimiento move1 = new Movimiento(1,"Latigo cepa",15,100,planta, 50);
+		
 		moveset.add(move1);
-		Movimiento move2 = new Movimiento(2,"Placaje",40,100,normal, 40);
 		moveset.add(move2);
-		Movimiento move3 = new Movimiento(3,"Absorber",20,100,planta, 20);
 		moveset.add(move3);
-		Movimiento move4 = new Movimiento(4,"Bomba Lodo",15,100,veneno, 90);
 		moveset.add(move4);
 		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
 		
-		ArrayList<Movimiento> moveset2 = new ArrayList<Movimiento>();
+
 		moveset2.add(move1);
 		moveset2.add(move2);
 		moveset2.add(move3);
 		moveset2.add(move4);
 		Pokemon pokemon2 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
 		
-		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
 		pokemon.add(pokemon1);
 		pokemon.add(pokemon2);
 		
-		Caja caja = new Caja(1, pokemon);
-		Caja caja2 = new Caja(1, pokemon);
-		Caja caja3 = caja;
+
 		assertEquals(caja.getId_caja(),1);
-		assertEquals(caja.getPokemon().get(1),pokemon2);
-		assertEquals(caja.toString(),caja2.toString());
-		assertEquals(caja.hashCode(),caja2.hashCode());
+	}
+	
+	@Test
+	void testCajaSetGet() {
 		
+		moveset2.add(move1);
+		moveset2.add(move2);
+		moveset2.add(move3);
+		moveset2.add(move4);
+		Pokemon pokemon2 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
+		assertEquals(caja.getPokemon().get(1),pokemon2);
+		assertEquals(caja.getId_caja(),1);
+		assertEquals(caja.setId_caja(6),6);
+		assertEquals(caja.setPokemon(pokemon),pokemon);
+	}
+	
+	@Test
+	void testCajaEquals() {
 		caja.equals(caja2);
 		caja.equals(null);
 		caja.equals(caja3);
@@ -62,5 +77,14 @@ class CajaTest {
 		caja.equals(caja2);
 		
 	}
+	
+	@Test
+	void testCajaToString() {
+		assertEquals(caja.toString(),caja.toString());
+	}
 
+	@Test
+	void testCajaHashCode() {
+		assertEquals(caja.hashCode(),caja.hashCode());
+	}
 }
--- Grupo2Reto4/src/test/JugadorTest.java
+++ Grupo2Reto4/src/test/JugadorTest.java
@@ -14,27 +14,31 @@ import modelo.Pokemon;
 import modelo.Tipo;
 
 class JugadorTest {
+	Tipo[] tipos = new Tipo[2];
+	Tipo planta = new Tipo(3, "Planta");
+	Tipo veneno = new Tipo(12, "Veneno");
+	Tipo normal = new Tipo(1, "Normal");
+	Movimiento move1 = new Movimiento(1, "Latigo cepa", 15, 100, planta, 50);
+	Movimiento move2 = new Movimiento(2, "Placaje", 40, 100, normal, 40);
+	Movimiento move3 = new Movimiento(3, "Absorber", 20, 100, planta, 20);
+	Movimiento move4 = new Movimiento(4, "Bomba Lodo", 15, 100, veneno, 90);
+	ArrayList<Movimiento> moveset2 = new ArrayList<Movimiento>();
+	ArrayList<Movimiento> moveset = new ArrayList<Movimiento>();
+	ArrayList<Caja> cajas = new ArrayList<Caja>();
 
 	@Test
-	void testJugador() {
-		Tipo[] tipos = new Tipo[2];
-		Tipo planta = new Tipo(3, "Planta");
-		Tipo veneno = new Tipo(12, "Veneno");
-		Tipo normal = new Tipo(1, "Normal");
+	void testJugadorConstructor() {
+
 		tipos[0] = planta;
 		tipos[1] = veneno;
-		ArrayList<Movimiento> moveset = new ArrayList<Movimiento>();
-		Movimiento move1 = new Movimiento(1, "Latigo cepa", 15, 100, planta, 50);
+
 		moveset.add(move1);
-		Movimiento move2 = new Movimiento(2, "Placaje", 40, 100, normal, 40);
 		moveset.add(move2);
-		Movimiento move3 = new Movimiento(3, "Absorber", 20, 100, planta, 20);
 		moveset.add(move3);
-		Movimiento move4 = new Movimiento(4, "Bomba Lodo", 15, 100, veneno, 90);
 		moveset.add(move4);
+
 		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
 
-		ArrayList<Movimiento> moveset2 = new ArrayList<Movimiento>();
 		moveset2.add(move1);
 		moveset2.add(move2);
 		moveset2.add(move3);
@@ -44,28 +48,81 @@ class JugadorTest {
 		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
 		pokemon.add(pokemon1);
 		pokemon.add(pokemon2);
-		Pokemon[] equipo = {null, pokemon1, pokemon2, null, null, null};
-		Pokemon[] equipo2 = {null, null, null, null, null, null};
+		Pokemon[] equipo = { null, pokemon1, pokemon2, null, null, null };
+		Pokemon[] equipo2 = { null, null, null, null, null, null };
 		Caja caja = new Caja(1, pokemon);
 
-		ArrayList<Caja> cajas = new ArrayList<Caja>();
 		cajas.add(caja);
 
 		MiPc pc1 = new MiPc(cajas);
 		MiPc pc2 = new MiPc(cajas);
-		
+
+		Jugador player = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		assertEquals(player.getLogin(), "Unai");
+	}
+
+	@Test
+	void TestGetSetJugador() {
+		tipos[0] = planta;
+		tipos[1] = veneno;
+		moveset.add(move1);
+		moveset.add(move2);
+		moveset.add(move3);
+		moveset.add(move4);
+		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
+		moveset2.add(move1);
+		moveset2.add(move2);
+		moveset2.add(move3);
+		moveset2.add(move4);
+		Pokemon pokemon2 = new Pokemon(1, "Ivysaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
+		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
+		pokemon.add(pokemon1);
+		pokemon.add(pokemon2);
+		Pokemon[] equipo = { null, pokemon1, pokemon2, null, null, null };
+		Pokemon[] equipo2 = { null, null, null, null, null, null };
+		Caja caja = new Caja(1, pokemon);
+		cajas.add(caja);
+		MiPc pc1 = new MiPc(cajas);
+		MiPc pc2 = new MiPc(cajas);
 		Jugador player = new Jugador("Unai", "Akos", "12345", equipo, pc1);
 		Jugador player2 = new Jugador("Unai", "Akos", "12345", equipo, pc1);
 		Jugador player3 = player;
-		assertTrue(player.Validar());
-		assertEquals(player.getLogin(),"Akos");
-		assertEquals(player.getPass(),"12345");
-		assertEquals(player.getEquipo().toString(),equipo.toString());
-		assertEquals(player.getNombre(),"Unai");
-		assertEquals(player.getPc().toString(), pc2.toString());
-		assertEquals(player.toString(), player2.toString());
-		assertEquals(player.hashCode(), player2.hashCode());
-		
+		assertEquals(player.getLogin(), "Akos");
+		assertEquals(player.getPass(), "12345");
+		assertEquals(player.getEquipo().toString(), equipo.toString());
+		assertEquals(player.getNombre(), "Unai");
+		assertEquals(player.setLogin("AAAA"), "AAAA");
+		assertEquals(player.setNombre("AAAB"), "AAAB");
+		assertEquals(player.setPass("HOLA"), "HOLA");
+	}
+
+	@Test
+	void testEqualsJugador() {
+		tipos[0] = planta;
+		tipos[1] = veneno;
+		moveset.add(move1);
+		moveset.add(move2);
+		moveset.add(move3);
+		moveset.add(move4);
+		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
+		moveset2.add(move1);
+		moveset2.add(move2);
+		moveset2.add(move3);
+		moveset2.add(move4);
+		Pokemon pokemon2 = new Pokemon(1, "Ivysaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
+		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
+		pokemon.add(pokemon1);
+		pokemon.add(pokemon2);
+		Pokemon[] equipo = { null, pokemon1, pokemon2, null, null, null };
+		Pokemon[] equipo2 = { null, null, null, null, null, null };
+		Caja caja = new Caja(1, pokemon);
+		cajas.add(caja);
+		MiPc pc1 = new MiPc(cajas);
+		MiPc pc2 = new MiPc(cajas);
+		Jugador player = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		Jugador player2 = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		Jugador player3 = player;
+
 		player.equals(null);
 		player.equals(player3);
 		player.equals(player2);
@@ -74,7 +131,7 @@ class JugadorTest {
 		player.equals(player2);
 		player2 = new Jugador("Unai", "Akos", "12345", null, pc1);
 		player.equals(player2);
-		
+
 		player2 = new Jugador("Unai", "Akos", "12345", null, null);
 		assertFalse(player2.Validar());
 		player2 = new Jugador("Unai", "Akos", "12345", equipo2, null);
@@ -82,4 +139,87 @@ class JugadorTest {
 
 	}
 
+	@Test
+	void testToStringJugador() {
+		tipos[0] = planta;
+		tipos[1] = veneno;
+		moveset.add(move1);
+		moveset.add(move2);
+		moveset.add(move3);
+		moveset.add(move4);
+		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
+		moveset2.add(move1);
+		moveset2.add(move2);
+		moveset2.add(move3);
+		moveset2.add(move4);
+		Pokemon pokemon2 = new Pokemon(1, "Ivysaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
+		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
+		pokemon.add(pokemon1);
+		pokemon.add(pokemon2);
+		Pokemon[] equipo = { null, pokemon1, pokemon2, null, null, null };
+		Pokemon[] equipo2 = { null, null, null, null, null, null };
+		Caja caja = new Caja(1, pokemon);
+		cajas.add(caja);
+		MiPc pc1 = new MiPc(cajas);
+		MiPc pc2 = new MiPc(cajas);
+		Jugador player = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		Jugador player2 = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		Jugador player3 = player;
+		assertEquals(player.getPc().toString(), pc2.toString());
+		assertEquals(player.toString(), player2.toString());
+	}
+
+	@Test
+	void testHashCodeJugador() {
+		tipos[0] = planta;
+		tipos[1] = veneno;
+		moveset.add(move1);
+		moveset.add(move2);
+		moveset.add(move3);
+		moveset.add(move4);
+		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
+		moveset2.add(move1);
+		moveset2.add(move2);
+		moveset2.add(move3);
+		moveset2.add(move4);
+		Pokemon pokemon2 = new Pokemon(1, "Ivysaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
+		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
+		pokemon.add(pokemon1);
+		pokemon.add(pokemon2);
+		Pokemon[] equipo = { null, pokemon1, pokemon2, null, null, null };
+		Pokemon[] equipo2 = { null, null, null, null, null, null };
+		Caja caja = new Caja(1, pokemon);
+		cajas.add(caja);
+		MiPc pc1 = new MiPc(cajas);
+		MiPc pc2 = new MiPc(cajas);
+		Jugador player = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		Jugador player2 = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+
+		assertEquals(player.hashCode(), player2.hashCode());
+	}
+
+	@Test
+	void testValidarJugador() {
+		tipos[0] = planta;
+		tipos[1] = veneno;
+		moveset.add(move1);
+		moveset.add(move2);
+		moveset.add(move3);
+		moveset.add(move4);
+		Pokemon pokemon1 = new Pokemon(1, "bulbasaur", tipos, 20, 5, 11, 11, 9, 6, moveset);
+		moveset2.add(move1);
+		moveset2.add(move2);
+		moveset2.add(move3);
+		moveset2.add(move4);
+		Pokemon pokemon2 = new Pokemon(1, "Ivysaur", tipos, 20, 5, 11, 11, 9, 6, moveset2);
+		ArrayList<Pokemon> pokemon = new ArrayList<Pokemon>();
+		pokemon.add(pokemon1);
+		pokemon.add(pokemon2);
+		Pokemon[] equipo = { null, pokemon1, pokemon2, null, null, null };
+		Caja caja = new Caja(1, pokemon);
+		cajas.add(caja);
+		MiPc pc1 = new MiPc(cajas);
+		Jugador player = new Jugador("Unai", "Akos", "12345", equipo, pc1);
+		assertTrue(player.Validar());
+	}
 }
