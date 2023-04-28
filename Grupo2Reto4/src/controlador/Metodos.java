85f5f4b7201d7a884fbb6f270a0b8d9f2e1dd823
in1dm3 <in1dm3@HZ352012.elorrieta.int>
2023-04-28 09:36:16 +0200

    commit metodo moverpokemon

--- Grupo2Reto4/src/controlador/Metodos.java
+++ Grupo2Reto4/src/controlador/Metodos.java
@@ -2,12 +2,17 @@ package controlador;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import excepciones.ArrayListLlenoException;
 import excepciones.NotFoundException;
 import manager.ManagerCajas;
 import manager.ManagerMovimientos;
@@ -15,14 +20,20 @@ import manager.ManagerPC;
 import manager.ManagerPokemon;
 import manager.ManagerTipos;
 import modelo.Caja;
+import modelo.Jugador;
 import modelo.MiPc;
 import modelo.Movimiento;
 import modelo.Pokemon;
 import modelo.Tipo;
 import modelo.Usuario;
+import utils.DBConexion;
 
 public class Metodos {
-
+	
+	Connection conexion;
+	Statement comando;
+	ResultSet registro;
+	
 	// Comprueba que el string enviado es vacio
 	public boolean esVacio(String contenido) {
 		return contenido.equals("");
@@ -102,6 +113,35 @@ public class Metodos {
 		}
 	
 	}
+	
+	
+	public void moverPokemonCajaEquipo (Jugador j, Pokemon p, Caja c) throws ArrayListLlenoException, SQLException {
+		int tamanyo = j.getEquipo().size();
+		if(tamanyo < 6) {
+			c.getPokemon().remove(p);	
+			j.getEquipo().add(p);
+			
+			try {
+				conexion = DriverManager.getConnection(DBConexion.URL, DBConexion.USER, DBConexion.PASSW);
+				comando = conexion.createStatement();
+
+				comando.executeUpdate("update " + DBConexion.T_EQUIPOS + " set poke_id" +(tamanyo+1)+" = "+p.getId()+" where user_login = '"+j.getLogin()+"';");
+
+				comando.executeUpdate("delete from "+DBConexion.T_CAJAS+" where pc_box_id ="+c.getId_caja()+";");
+
+			} finally {
+				registro.close();
+				comando.close();
+				conexion.close();
+			}
+			
+		}else {
+			throw new ArrayListLlenoException("Equipo lleno");
+		}
+		
+	}
+	
+	
 	
 
 }
